c846603ecf10b9aa8c718f79c8fb13074e0587e4
Nuno Teixeira <nuno@codacy.com>
2015-04-06 16:03:38 +0100

    integers

--- src/main/java/codacy/api/model/CommitDelta.java
+++ src/main/java/codacy/api/model/CommitDelta.java
@@ -14,25 +14,25 @@ public class CommitDelta extends Commit {
     public class CommitDeltaData {
 
 
-        private String newIssues;
-        private String fixedIssues;
-        private String complexity;
-        private String nrClones;
+        private Integer newIssues;
+        private Integer fixedIssues;
+        private Integer complexity;
+        private Integer nrClones;
         private ArrayList<FileDelta> files;
 
-        public String getNewIssues() {
+        public Integer getNewIssues() {
             return newIssues;
         }
 
-        public String getFixedIssues() {
+        public Integer getFixedIssues() {
             return fixedIssues;
         }
 
-        public String getComplexity() {
+        public Integer getComplexity() {
             return complexity;
         }
 
-        public String getNrClones() {
+        public Integer getNrClones() {
             return nrClones;
         }
 
